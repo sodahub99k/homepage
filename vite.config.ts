64bcd0108c6de83ce64d9924da2792a034a5c6cd
sodahub-64 <sotarockyz@icloud.com>
2026-01-01 23:44:44 +0900

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import solid from 'vite-plugin-solid'
 
-export default defineConfig({
-  plugins: [tailwindcss(), solid()],
+export default defineConfig(({ mode }) => {
+  let base = "/"
+
+
+  if (mode === "production") {
+    base = "/homepage/"
+  }
+
+  return {
+    base,
+    plugins: [tailwindcss(), solid()],
+  }
 })
